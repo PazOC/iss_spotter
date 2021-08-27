c6af01988fc9f935ee6426010509076a52c82cbf
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-27 01:26:33 +0000

    ISS Spotter With Promises

---
+++ iss_promised.js
@@ -0,0 +1,18 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
